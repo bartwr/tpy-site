7e6866a814933315bab8d836ba62a6bd0699490b
bartwr <mail@bartroorda.nl>
2021-03-22 14:38:23 +0100

    Co-creation: Created co-creation page

--- pages/co-creation.js
+++ pages/co-creation.js
@@ -12,68 +12,51 @@ const BookATourBanner = dynamic(() => import('../components/book-a-tour-banner.j
 const FooterBanner = dynamic(() => import('../components/footer-banner.js'));
 const Footer = dynamic(() => import('../components/footer.js'));
 const NewsletterSubscribe = dynamic(() => import('../components/newsletter-subscribe.js'));
+const FullWidthImage = dynamic(() => import('../components/full-width-image.js'));
 
-function CoCreation() {
+function Cocreation() {
   return <div className="root">
     <Head>
-
-      <title>Co-Creation | The Hague Tech</title>
-
-      <meta key="og:title" property="og:title" content="Co-Creation | The Hague Tech" />
-      <meta key="og:image" property="og:image" content="https://www.thehaguetech.com/static/pages/co-creation/meta.jpg" />
-      <meta key="og:description" property="og:description" content="Working together to co-create prototypes, products, technological solutions, and more by collaborating with the talent all around The Hague Tech." />
-      <meta key="description" name="description" content="Working together to co-create prototypes, products, technological solutions, and more by collaborating with the talent all around The Hague Tech." />
-
+      <title key="title">Cocreation | Technology Park Ypenburg</title>
+      <meta key="og:title" property="og:title" content="Cocreation | Technology Park Ypenburg" />
+      <meta key="og:image" property="og:image" content="https://www.thehaguetech.com/static/pages/community/meta.jpg" />
+      <meta key="og:description" property="og:description" content="Build your network, build your business, and build your friendships all within our passionate community." />
+      <meta key="description" name="description" content="Build your network, build your business, and build your friendships all within our passionate community." />
     </Head>
-    <Navigation />
-    <PageHeader
-      smallCapsTitle="Co-creation"
-      title="Co-create and innovate with our multi-talented community"
-      image="/static/pages/co-creation/page-header.jpg"
-      />
-    <IntroText>
-      The Hague Tech is an inspiring place to collaborate with talented individuals and innovative companies. But we strive to be and do more, to nurture an environment of of co-creation. Co-creation means working together to create prototypes, products, technological solutions and beyond by activating the talent within The Hague Tech. Our community is filled with talented members looking to work together on new and exciting challenges. 
-    </IntroText>
-    {/*
-    Do you have an idea? 
-    -Bring your idea and form a team to create a solution.
-    Do you have a talent you want to share?
-    -Bring your talent and join a team working on innovative solutions. 
-    Do you have a problem? 
-    -As a corporate or an individual, bring your problem to our community to co-create a viable solution with talented individuals. 
-    */}
-    <TextAndImage
-      image="/static/pages/co-creation/cocreate.jpg"
-      category=""
-      title=""
-      text="The community (our members), our services, physical location, and hyperlocality of our community together form the innovation ecosystem we strive to create. The ecosystem empowers entrepreneurs and individuals to find and share opportunities, form cooperative partnerships, tackle obstacles, create personal contacts, transfer complex ideas and information, and make valuable connections between parties who otherwise would not be able to find each other."
-      />
-    {/*
-    (examples of co-creation) 
-    Samsung
-    Husense
-    DutchBudz
-    Soft Landing [Link to the International Business Landing Page]
-    */}
-    {/*
-    Graphic 
-    -Co-Creation Workflow
 
-    Bring a Problem -> Find a Project Manager -> Build a Team -> Create a Solution -> Deliver a Solution
-    */}
+    <Navigation />
+
+    <PageHeader
+      smallCapsTitle="Cocreation"
+      smallCapsTitleColor="white"
+      title="TPY Cocreation"
+      imageBk="/static/pages/community/community.jpg"
+      image="https://loremflickr.com/1920/1280"
+      style={{backgroundPosition: 'center top'}}
+    />
+
+    <IntroText>
+      <p>
+        In life together you are stronger and that is the same for business. Co-creation is a big pillar within our EcoSystem. We facilitate in various dimensions, to make sure our members have everything they need to improve and achieve common goals. 
+      </p>
+      <p>
+        We drive events which are educational in content or challenging through events like “Hackathons” and “Makethons”. In addition, we set up innovation projects for products and processes and help find the appropriate local, regional, national and european funding initiatives to help accelerate your development.
+      </p>
+    </IntroText>
+
     <div>
-      <YellowBanner
-        title="Come and see for your self what The Hague Tech has to offer you!"
-        buttonText="book a tour"
-        buttonLink="https://outlook.office365.com/owa/calendar/TheHagueTech@jongensvantechniek.nl/bookings/"
-        target="_blank"
-        />
+      <FooterBanner
+        title="Join our academy"
+        buttonText="send us a message"
+        buttonLink="/contact"
+      />
     </div>
+
     <NewsletterSubscribe />
-    <div>
-      <Footer />
-    </div>
+
+    <Footer />
+
   </div>
 }
 
-export default CoCreation;
+export default Cocreation;
