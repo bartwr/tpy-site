d252eec1d59cb8ca440792cdba068117554ff962
bartwr <mail@bartroorda.nl>
2021-03-25 12:10:03 +0100

    Created redirects (old site URLs -> new site URLs)

--- next.config.js
+++ next.config.js
@@ -2,21 +2,65 @@ require('dotenv').config();
 const webpack = require('webpack');
 
 module.exports = {
-    webpack: (config, { dev }) => {
-        config.plugins.push(
-            new webpack.ProvidePlugin({
-                '$': 'jquery',
-                'jQuery': 'jquery',
-            })
-        );
-        config.plugins.push(
-            new webpack.DefinePlugin({
-                SPACE_ID: JSON.stringify(process.env.CONTENTFUL_SPACE_ID),
-                ACCESS_TOKEN: JSON.stringify(process.env.CONTENTFUL_ACCESS_TOKEN),
-            })
-        );
-        config.optimization.minimize = false;
-        config.node.fs = "empty";
-        return config
-    }
+  async redirects() {
+    return [
+      {
+        source: '/services',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/gallery',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/tpy-projects',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/kunststof-magazine-articles',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/tpy-experts-explain',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/events-2',
+        destination: '/happening',
+        permanent: true,
+      },
+      {
+        source: '/fotos-update-event-27-juni-2019',
+        destination: '/happening',
+        permanent: true,
+      },
+      {
+        source: '/in-the-media',
+        destination: '/happening',
+        permanent: true,
+      }
+    ]
+  },
+  webpack: (config, { dev }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        '$': 'jquery',
+        'jQuery': 'jquery',
+      })
+    );
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        SPACE_ID: JSON.stringify(process.env.CONTENTFUL_SPACE_ID),
+        ACCESS_TOKEN: JSON.stringify(process.env.CONTENTFUL_ACCESS_TOKEN),
+      })
+    );
+    config.optimization.minimize = false;
+    config.node.fs = "empty";
+    return config
+  }
 };
