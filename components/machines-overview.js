d74d9629e6280943cb41975653ca9e74e2e99c03
bartwr <mail@bartroorda.nl>
2022-03-28 22:12:47 +0200

    Machine Overview: Added some more transitions

--- components/machines-overview.js
+++ components/machines-overview.js
@@ -38,26 +38,23 @@ const MachineQuickView = (props) => {
     z-10
     max-h-full
     overflow-auto
-  ">
-    <motion.div
-      layout
-      initial={{ opacity: 0.5 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.15 }}
-    >
-      <div className="
-        absolute
-        top-0
-        right-0
-        bottom-0
-        left-0
-      " style={{
-        backgroundColor: 'rgba(241, 239, 236, 0.8)'
-      }}
-      onClick={closeHandler}
-      />
-    </motion.div>
+  "
+  >
+    <motion.div className="
+      absolute
+      top-0
+      right-0
+      bottom-0
+      left-0
+    " style={{
+      backgroundColor: 'rgba(241, 239, 236, 0.8)'
+    }}
+    onClick={closeHandler}
+    animate={{ opacity: 1 }}
+    initial={{ opacity: 0 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.2 }}
+    />
 {/*    <motion.div
       animate={{ scale: 2 }}
       transition={{ duration: 0.5 }}
@@ -70,13 +67,11 @@ const MachineQuickView = (props) => {
     >
 */}
     <motion.div
-      key={true}
-      animate={{ opacity: 1, y: 0 }}
-      initial={{ opacity: 0, y: 20 }}
-      exit={{ opacity: 0, y: -20 }}
-      transition={{ duration: 0.15 }}
-    >
-      <div className="
+      animate={{ opacity:1, scale:1 }}
+      initial={{ opacity:0.6, scale:0.6 }}
+      exit={{ opacity:0 }}
+      transition={{ duration: 0.2 }}
+      className="
         MachineQuickView-inner
         relative
         bg-white
@@ -91,49 +86,49 @@ const MachineQuickView = (props) => {
       " style={{
         maxWidth: '872px',
         width: '100%'
-      }}>
-        <div className="
-          w-full
-          md:w-2/5
-          mb-8
-          md:mb-0
-        ">
-          <img src="https://i.imgur.com/83Ovq2V.jpeg" />
-        </div>
-        <div className="
-          w-full
-          md:w-3/5
-        ">
-          <div className="pl-4 pr-4 md:pl-14">
-            <Title style={{marginBottom: '20px'}}>
-              {machine.title}
-            </Title>
-            <MachineSpecifications machine={machine} />
-            <p className="my-2 mt-4">
-              <a href="/machines-details" className="
-                text-theme-orange
-                text-base
-                no-underline
-              ">
-                View full item
-              </a>
-            </p>
-            <div className="
-              flex
-              justify-between
-              mt-12
-              w-full
+      }}
+    >
+      <div className="
+        w-full
+        md:w-2/5
+        mb-8
+        md:mb-0
+      ">
+        <img src="https://i.imgur.com/83Ovq2V.jpeg" />
+      </div>
+      <div className="
+        w-full
+        md:w-3/5
+      ">
+        <div className="pl-4 pr-4 md:pl-14">
+          <Title style={{marginBottom: '20px'}}>
+            {machine.title}
+          </Title>
+          <MachineSpecifications machine={machine} />
+          <p className="my-2 mt-4">
+            <a href="/machines-details" className="
+              text-theme-orange
+              text-base
+              no-underline
             ">
-              <div>
-                <Button target="_blank" buttonLink={'#'}>
-                  contact
-                </Button>
-              </div>
-              <div className="flex flex-col justify-center">
-                <a href="#" onClick={closeHandler}>
-                  Sluiten
-                </a>
-              </div>
+              View full item
+            </a>
+          </p>
+          <div className="
+            flex
+            justify-between
+            mt-12
+            w-full
+          ">
+            <div>
+              <Button target="_blank" buttonLink={'#'}>
+                contact
+              </Button>
+            </div>
+            <div className="flex flex-col justify-center">
+              <a href="#" onClick={closeHandler}>
+                Sluiten
+              </a>
             </div>
           </div>
         </div>
@@ -147,134 +142,152 @@ const Machine = (props) => {
 
   const [doShowQuickView, setDoShowQuickView] = useState(false)
 
-  return <div className="
-    Machine
-    my-4
-    w-full
-    md:w-1/2
-    lg:w-1/3
-    my-2
-    md:my-5
-  "
-  key={data.title}
-  style={{
-    minHeight: '500px'
-  }}
-  >
-    <div className="
-      Machine-inner
-      h-full
-      flex
-      flex-col
-      md:mx-4
-      relative
-    " style={{
-      boxShadow: '0px 2px 13px rgba(0, 0, 0, 0.2)'
-    }}>
-      <div className="
-        Machine-image
-        bg-center
-        bg-no-repeat
-        bg-cover
-        w-full
-        h-48
-        mx-auto
-      " style={{
-        backgroundImage: `url('${data.image}')`
-      }} />
-      <div className="
-        Machine-quickView
-        uppercase
-        font-montserrat
-        text-md
-        text-white
-        opacity-90
-        inline-block
-        mx-auto
-        w-36
-        h-12
-        text-center
+  return (
+    <div
+    className="
+      Machine
+      my-4
+      w-full
+      md:w-1/2
+      lg:w-1/3
+      my-2
+      md:my-5
+    "
+    style={{
+      minHeight: '500px'
+    }}
+    >
+      <motion.div
+      whileHover={{
+        boxShadow: '0px 8px 13px rgba(0, 0, 0, 0.2)'
+      }}
+      layoutId="underline"
+      className="
+        Machine-inner
+        h-full
         flex
         flex-col
-        justify-center
-        cursor-pointer
-        -mt-6
-      " style={{
-        backgroundColor: '#BAB1A1'
-      }}
-      onClick={() => setDoShowQuickView(true)}
-      >
-        quick view
-      </div>
-      <div className="
-        Machine-body
-        py-6
-        px-10
-      ">
-        <span className="
-          text-theme-green
-          font-bold
-          text-sm
-          uppercase
-          inline-block
-          font-montserrat
-        ">
-          {data.machineType}
-        </span>
-        <h1 className="
-          Machine-title
-          text-xl
-          font-semibold
-          font-montserrat
-          text-theme-blue
-          my-2
-        ">
-          {data.title}
-        </h1>
+        md:mx-4
+        relative
+      "
+      style={{
+        boxShadow: '0px 2px 13px rgba(0, 0, 0, 0.2)'
+      }}>
         <div className="
-          text-theme-black
-        ">
-          {data.description}
-        </div>
-      </div>
-      <footer className="
-        flex font-montserrat
-        absolute
-        bottom-0
-        w-full
-      ">
-        <a className="
-          flex-1
-          bg-white
-          text-center
-          flex
-          flex-col
-          justify-center
-          h-11
-          text-orange
-          cursor-pointer
-        " href="/machines-details">
-          more info
-        </a>
-        <a className="
-          flex-1
-          bg-theme-orange
-          text-center
-          flex
-          flex-col
-          justify-center
-          h-11
+          Machine-image
+          bg-center
+          bg-no-repeat
+          bg-cover
+          w-full
+          h-48
+          mx-auto
+        " style={{
+          backgroundImage: `url('${data.image}')`
+        }} />
+        <motion.div className="
+          Machine-quickView
+          uppercase
+          font-montserrat
+          text-md
           text-white
+          opacity-90
+          inline-block
+          mx-auto
+          w-36
+          h-12
+          text-center
+          flex
+          flex-col
+          justify-center
           cursor-pointer
-        " href="/machines-details">
-          contact
-        </a>
-      </footer>
+          -mt-6
+        " style={{
+          backgroundColor: '#BAB1A1'
+        }}
+        onClick={() => setDoShowQuickView(true)}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        >
+          quick view
+        </motion.div>
+        <div className="
+          Machine-body
+          py-6
+          px-10
+        ">
+          <span className="
+            text-theme-green
+            font-bold
+            text-sm
+            uppercase
+            inline-block
+            font-montserrat
+          ">
+            {data.machineType}
+          </span>
+          <h1 className="
+            Machine-title
+            text-xl
+            font-semibold
+            font-montserrat
+            text-theme-blue
+            my-2
+          ">
+            {data.title}
+          </h1>
+          <div className="
+            text-theme-black
+          ">
+            {data.description}
+          </div>
+        </div>
+        <footer className="
+          flex font-montserrat
+          absolute
+          bottom-0
+          w-full
+        ">
+          <motion.a className="
+            flex-1
+            bg-white
+            text-center
+            flex
+            flex-col
+            justify-center
+            h-11
+            text-orange
+            cursor-pointer
+          "
+          href="/machines-details"
+          whileHover={{ fontStyle: 'italic' }}
+          whileTap={{ fontStyle: 'italic' }}
+          >
+            more info
+          </motion.a>
+          <motion.a className="
+            flex-1
+            bg-theme-orange
+            text-center
+            flex
+            flex-col
+            justify-center
+            h-11
+            text-white
+            cursor-pointer
+          "
+          href="/machines-details"
+          whileHover={{ fontStyle: 'italic' }}
+          whileTap={{ fontStyle: 'italic' }}
+          >
+            contact
+          </motion.a>
+        </footer>
+      </motion.div>
+      {doShowQuickView && <MachineQuickView onClose={
+        () => {setDoShowQuickView(false)}
+      } machine={data} />}
     </div>
-    {doShowQuickView && <MachineQuickView onClose={
-      () => {setDoShowQuickView(false)}
-    } machine={data} />}
-  </div>
+  )
 }
 
 const MachinesOverview = (props) => {
